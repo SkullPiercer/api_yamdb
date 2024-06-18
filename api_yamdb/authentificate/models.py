47debd325e66d703c111df9f603b6084dbffe52f
Александр Денисов <akatsuki_sanya@vk.com>
2024-06-18 10:46:46 +0500

    Remade CustomUser model

--- api_yamdb/authentificate/models.py
+++ api_yamdb/authentificate/models.py
@@ -3,10 +3,13 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True, max_length=254)
+    bio = models.TextField(blank=True, null=True)
     role = models.CharField(max_length=50, choices=(
         ('user', 'User'),
         ('moderator', 'Moderator'),
         ('admin', 'Admin'),
     ), default='user')
+
+    class Meta:
+        ordering = ('email',)
